1e62cb9c2ce65231ccb28e5deb9db5b99a6ad9f6
pai <hachikopolarcito321@gmail.com>
2021-06-21 10:19:25 -0500

    delete script.js

--- public/script.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-    var x = document.forms["myForm"]["message"].value;
-    if (x == "") {
-      alert("no puedes enviar un mensaje vacio!");
-      return false;
-    }
-  }
