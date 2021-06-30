5017988cb048f6cb96c9947b9171a3d27a87926a
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-06-30 17:59:42 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,6 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(require("./routes/deno"));
+app.use(require("./routes/index"));
 app.use(express.static(join(__dirname, "public")));
 app.listen(PORT, () => console.log(`server on http://localhost:${PORT}`));
