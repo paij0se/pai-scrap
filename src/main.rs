41ae1d7708bdb6601b96e9e6d23baa1ca5972568
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-06-30 17:56:21 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,27 @@ use std::process::{Command, Stdio};
 use std::thread;
 
 fn threads() {
-    let test = env::var("URL").expect("Expected a URL");
-    const NTHREADS: u32 = 2; // number of threads
+    let url = env::var("URL").expect("Expected a URL");
+    const NTHREADS: u32 = 1; // number of threads
     let mut children = vec![];
 
     for _i in 0..NTHREADS {
-        let test = test.clone();
+        let url = url.clone();
         children.push(thread::spawn(move || {
             let ou = Command::new("curl")
-                .arg(test)
+                .arg(url)
                 .stdout(Stdio::piped())
                 .output()
                 .expect("Failed to execute command");
-
-            //let re = Regex::new(r"(https?://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?)",).unwrap(); // match all the https and http links
-            let re = Regex::new(r"(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)").unwrap(); // match all the .png, .jpg, .gif https and http links
+            let re = Regex::new(r"(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|jpeg|png)").unwrap();
             let output = String::from_utf8(ou.stdout).unwrap();
-            let formated = re.find_iter(&output);
-            for i in formated {
-                println!("{}", i.as_str())
+            let formated = re.find_iter(&output).collect::<Vec<_>>();
+            if formated.is_empty() {
+                println!("No matches from the URL");
+            } else {
+                for i in formated {
+                    println!("{}", i.as_str())
+                }
             }
         }));
     }
