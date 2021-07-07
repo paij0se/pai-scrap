1b3c44ad01cce6ed8ae6c65b776488e0473ecf88
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-07-06 23:16:38 -0500

    Update index.ts

--- routes/index.ts
+++ routes/index.ts
@@ -6,29 +6,17 @@ const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 60, // limit 10 request for minute
-  message: ({out:"HTTP ERROR 429 to many requests"}),
+  message: { out: "HTTP ERROR 429 to many requests" },
 });
 
-router.post("/url-sent",limiter, (req, res) => {
+router.post("/url-sent", limiter, (req, res) => {
   const { url } = req.body;
-  console.log(url);
-  exec(`URL=${url} ./multithreading`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`error: ${error.message}`);
-      return;
-    }
-
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-      return;
-    }
-
-    console.log(`${stdout}`);
-      res.send(
-        `<h1 align="center">(jpg,png,gif,jpeg), scrapping: <h2 align="center">${url}</h2></h1> <p align="center">${stdout}</p>`
-      );
+  console.log(`input: ${url}`);
+  exec(`URL=${url} ./multithreading`, (_, stdout, stderr) => {
+    res.send(
+      `<h1 align="center">(jpg,png,gif,jpeg), scrapping: <h2 align="center">${url}</h2></h1> <p align="center">${stdout}</p>`
+    );
   });
 });
 
 module.exports = router;
-
