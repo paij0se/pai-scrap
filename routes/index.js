c2dda91fbabd4bd304dda0bb554026796d6dbfab
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-07-06 23:17:12 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,12 @@ var rateLimit = require("express-rate-limit");
 var limiter = rateLimit({
     windowMs: 1 * 60 * 1000,
     max: 60,
-    message: ({ out: "HTTP ERROR 429 to many requests" })
+    message: { out: "HTTP ERROR 429 to many requests" }
 });
 router.post("/url-sent", limiter, function (req, res) {
     var url = req.body.url;
-    console.log(url);
-    exec("URL=" + url + " ./multithreading", function (error, stdout, stderr) {
-        if (error) {
-            console.error("error: " + error.message);
-            return;
-        }
-        if (stderr) {
-            console.error("stderr: " + stderr);
-            return;
-        }
-        console.log("" + stdout);
+    console.log("input: " + url);
+    exec("URL=" + url + " ./multithreading", function (_, stdout, stderr) {
         res.send("<h1 align=\"center\">(jpg,png,gif,jpeg), scrapping: <h2 align=\"center\">" + url + "</h2></h1> <p align=\"center\">" + stdout + "</p>");
     });
 });
